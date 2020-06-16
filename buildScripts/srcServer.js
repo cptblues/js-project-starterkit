0c4e3f8403d8be133b580485f07409e5a95478b1
Kévin Poulet <p.kevin11120@gmail.com>
2020-06-16 09:22:52 +0200

    save project

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 1,"firstName":"Tammy","lastName":"Norton","email":"tnorton@gmail.com"},
+    {"id": 1,"firstName":"Tina","lastName":"Lee","email":"lee.tina@gmail.com"},
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     /* eslint-disable no-console */
